ea32745697cb9070ce17652027dd21854a216966
Tijl <tijl@houtbecke.rs>
2023-10-23 10:18:09 +0200

    probot-settings fork update 2

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = require('probot-settings');
 
 // Adapt the Probot app for Actions
 // This also acts as the main entrypoint for the Action
+console.debug(`app = ${app}`);
 run(app).catch((error) => {
-  console.debug(`app = ${app}`);
   console.error(error);
   process.exit(1);
 });
